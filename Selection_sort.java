2548db13a0c9c191fbaf4cf5d0871249d146b52a
Anant Chaudhary <78264061+anant0070@users.noreply.github.com>
2021-10-13 13:01:07 +0530

    Update Selection_sort.java

--- Selection_sort.java
+++ Selection_sort.java
@@ -1,27 +1,36 @@
+// Selection sort in Java
+
 import java.util.Arrays;
 
-public class SelectionSort {
+class SelectionSort {
+  void selectionSort(int array[]) {
+    int size = array.length;
 
-    void selectionSort(int array[]) {
-        int size = array.length;
-        for (int step = 0; step < size - 1; step++) {
-            int min_idx = step;
-            for (int i = step + 1; i < size; i++) {
-                if (array[i] < array[min_idx]) {
-                    min_idx = i;
-                }
-            }
+    for (int step = 0; step < size - 1; step++) {
+      int min_idx = step;
 
-            int temp = array[step];
-            array[step] = array[min_idx];
-            array[min_idx] = temp;
+      for (int i = step + 1; i < size; i++) {
+
+        // To sort in descending order, change > to < in this line.
+        // Select the minimum element in each loop.
+        if (array[i] < array[min_idx]) {
+          min_idx = i;
         }
+      }
+
+      // put min at the correct position
+      int temp = array[step];
+      array[step] = array[min_idx];
+      array[min_idx] = temp;
     }
-    public static void main(String args[]) {
-        int[] arr = {45,25,89,65,24,2, 4,55,656, 3, 5, 1, 8, 6, 7};
-        SelectionSort temp = new SelectionSort();
-        temp.selectionSort(arr);
-        System.out.println("Sorted Array in Ascending Order: ");
-        System.out.println(Arrays.toString(arr));
-    }
+  }
+
+  // driver code
+  public static void main(String args[]) {
+    int[] data = { 20, 12, 10, 15, 2 };
+    SelectionSort ss = new SelectionSort();
+    ss.selectionSort(data);
+    System.out.println("Sorted Array in Ascending Order: ");
+    System.out.println(Arrays.toString(data));
+  }
 }
