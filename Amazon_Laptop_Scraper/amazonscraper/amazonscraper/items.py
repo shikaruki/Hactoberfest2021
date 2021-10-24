3c488cfe26a5eda84e2d9c015227e878eb9dd4f7
akash <das88768@gmail.com>
2021-10-24 22:28:46 +0530

    Ready to deploy the main py file and readme file

---
+++ Amazon_Laptop_Scraper/amazonscraper/amazonscraper/items.py
@@ -0,0 +1,29 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose, Join
+from w3lib.html import remove_tags
+
+def remove_currency(value):
+    return value.replace('₹', '')
+
+class AmazonscraperItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field(
+        input_processor = MapCompose(remove_tags),
+        output_processor = TakeFirst()
+    )
+    price = scrapy.Field(
+        input_processor = MapCompose(remove_tags, remove_currency),
+        output_processor = TakeFirst()
+    )
+    discprice = scrapy.Field(
+        output_processor = TakeFirst()
+    )
+    link = scrapy.Field(
+        output_processor = TakeFirst()
+    )
