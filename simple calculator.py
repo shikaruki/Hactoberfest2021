e73fdf40c1d805a0dc5b2b0cceee0e321611a9d9
Madhuvantha K.A.N <37581397+Nawod@users.noreply.github.com>
2021-10-29 12:06:19 +0530

    Update simple calculator.py
    Update the simple calculator by increasing user experience

--- simple calculator.py
+++ simple calculator.py
@@ -1,54 +1,52 @@
+#hacktoberfest-accepted
+#Simple Calculator
+
+#define add function
 def add(x, y):
     return x + y
 
-
-
+#define subtract function
 def subtract(x, y):
     return x - y
 
-
-
+#define multiply function
 def multiply(x, y):
     return x * y
 
-
-
+#define divide function
 def divide(x, y):
     return x / y
 
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
 while True:
+    #get number 1
+    num1 = float(input("Enter first number : "))
+    choice = input("Enter the operation (+,-,/,*) : ")
+    
+    #check wether the operation inputs are valid
+    if choice in ('+', '-', '/', '*'):
+        #get number 2
+        num2 = float(input("Enter second number : "))
 
-    choice = input("Enter choice(1/2/3/4): ")
-
-
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if choice == '1':
+        #call the functions
+        if choice == '+':
             print(num1, "+", num2, "=", add(num1, num2))
 
-        elif choice == '2':
+        elif choice == '-':
             print(num1, "-", num2, "=", subtract(num1, num2))
 
-        elif choice == '3':
+        elif choice == '*':
             print(num1, "*", num2, "=", multiply(num1, num2))
 
-        elif choice == '4':
+        elif choice == '/':
             print(num1, "/", num2, "=", divide(num1, num2))
-
-
-        
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-            break
 
     else:
         print("Invalid Input")
+    
+    #continue or break the program
+    next_calculation = input("Do you want to continue? (yes/no): ")
+    if next_calculation == "no":
+        break
+
+    
