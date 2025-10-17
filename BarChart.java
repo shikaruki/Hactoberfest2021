ec74d3811b426c9eb8f495f3fdc89cafea216b88
Harsh Mishra <148937385+harshmishra00@users.noreply.github.com>
2025-10-17 10:53:41 +0530

    Refactor BarChart for improved readability and efficiency

--- BarChart.java
+++ BarChart.java
@@ -1,43 +1,19 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class BarChart {
-	
-	public static void main(String[] args) {
-		
-		Scanner scn = new Scanner(System.in);
-		
-		int tc = scn.nextInt();
-		
-		int arr[] = new int[tc];
-		
-		
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = scn.nextInt();
-			 if(arr[i]>max)
-			 {
-				 max = arr[i];
-			 }
-		}	
-		
-		for(int floor = max ; floor>=1 ; floor--)
-		{
-			for(int i = 0 ; i<arr.length ; i++)
-			{
-				
-				if(arr[i]>=floor)
-				{
-					System.out.print("*\t");
-				}
-				else
-				{
-					System.out.print("\t");
-				}
-				
-			}
-			System.out.println();
-		}
-		
-	}
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int[] heights = new int[n];
+            for (int i = 0; i < n; i++) heights[i] = sc.nextInt();
 
+            int max = Arrays.stream(heights).max().orElse(0);
+            for (int level = max; level >= 1; level--) {
+                for (int h : heights)
+                    System.out.print(h >= level ? "* " : "  ");
+                System.out.println();
+            }
+        }
+    }
 }
